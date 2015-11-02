7fd6bd61c83dcf192f1a71a719f2796718b3e4c3
Casey Koellner <koellner.casey73@gmail.com>
2015-11-02 12:59:51 -0500

    Players can place their pieces

--- game.rb
+++ game.rb
@@ -36,36 +36,84 @@ class Game
 		print("\n\n")
 	end
 
-	def Game.input
+	def Game.input(player, piece)
+		choice = -1
+		while choice.to_i < 1 || choice.to_i > 7 
+			print("#{player} choose a slot (1-7): ")
+			choice = gets.chomp.to_i
+		end
+
+		column = Game.get_column(choice)
+		if(column.empty?)
+			puts "Full column"
+			Game.input(player,piece)
+		end
+
+		queue = []
+		column.each do |x|
+			queue << x
+		end
+		#Flips queue so that the end of the column is the first checked
+		queue.reverse!
+		#Places a piece in the #choice column 
+		until queue.empty?
+			location = queue.shift
+			if(@@board[location] == "|#{choice}|")
+				@@board[location] = "|#{piece}|"
+				return true
+			else next
+			end
+		end
 
 	end
 
-	def Game.check_win
+	def Game.get_column(column)
+		list = []
+		index = column
+		@@board.each do |location,x|
+			#Adds the location of each board piece in the column
+			if(x == "|#{index}|")
+				list << location
+			end
+		end
+		return list
+	end
 
+	def Game.check_win(player)
+		
 	end
 
 	def Game.check_tie
-
+		#If there are any remaining open slots, return false
+		@@board.each do |location,x|
+			if(x == "|1|" || x == "|2|" || x == "|3|" || x == "|4|" || x == "|5|" || x == "|6|" || x == "|7|")
+				return false
+			end
+		end
+		#All slots are filled, return true
+		return true
 	end
 
 	def play
-		show_board(@@board, 7)
 		p1 = 0
 		p2 = 0
 		winner = ""
 
 		#If player 1 wins, check_win = 1, if player 2 wins, check_win = 2.
 		until Game.check_win == 1 || Game.check_win == 2 || Game.check_tie do
+			show_board(@@board, 7)
+						
+			p1 = Game.input("Player 1", "o")
+			Game.check_win("|o|")
 			Game.check_tie
-			
-			p1 = Game.input
-			p2 = Game.input
-
-			Game.check_win
-
+			p2 = Game.input("Player 2", "x")
+			Game.check_win("|x|")		
+			Game.check_tie
 		end
 
-
+		if(Game.check_tie)
+			print("\nGame resulted in a tie")
+		end
 	end
 end
 
